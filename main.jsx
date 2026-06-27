44928958376bc5c86b00b2230681e1e422e9912e
framesrate <framesrate1@gmail.com>
2026-06-27 16:36:32 +0530

    Replace App component with GamberValleyMIS
    Replaced App component with GamberValleyMIS component in main.jsx.

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App.jsx"
+import GamberValleyMIS from './GamberValleyMIS_Final (1).jsx';
 
 const rootElement = document.getElementById("root")
 
